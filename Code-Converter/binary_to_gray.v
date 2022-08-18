21af5f3377177868f48d28a0578763ccb6311c90
Zannatul Naim <zannatulnaim09@gmail.com>
2022-08-18 09:34:15 +0600

    Binary to Gray

---
+++ Code-Converter/binary_to_gray.v
@@ -0,0 +1,11 @@
+module binary_to_gray(a, b);
+    
+    input [3:0] a;
+    output [3:0] b;
+    
+    assign b[3] = a[3];
+    assign b[2] = a[3]^a[2];
+    assign b[1] = a[2]^a[1];
+    assign b[0] = a[1]^a[0];
+    
+endmodule;
