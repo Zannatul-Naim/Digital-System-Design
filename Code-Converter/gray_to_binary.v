f23aaf4bc73fc383a60fe50ce64313c76887ec7e
Zannatul Naim <zannatulnaim09@gmail.com>
2022-08-18 09:37:17 +0600

    Gray to Binary code conversion

---
+++ Code-Converter/gray_to_binary.v
@@ -0,0 +1,10 @@
+module gray_to_binary(a, b);
+    input [3:0] a;
+    output [3:0] b;
+    
+    assign b[3] = a[3];
+    assign b[2] = b[3]^a[2];
+    assign b[1] = b[2]^a[1];
+    assign b[0] = b[1]^a[0];
+    
+endmodule
