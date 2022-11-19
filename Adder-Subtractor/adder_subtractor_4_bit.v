97ee7d54641cd4a7c7763d631fcf0440e5c912d8
Zannatul Naim <zannatulnaim09@gmail.com>
2022-11-19 16:35:20 +0600

    bug fixed

--- Adder-Subtractor/adder_subtractor_4_bit.v
+++ Adder-Subtractor/adder_subtractor_4_bit.v
@@ -5,12 +5,13 @@ module adder_subtractor(s, cout, a, b, mode);
 	output [3:0] s;
 	output cout;
 	
-	wire c1, c2, c3;
+	wire c1, c2, c3, c4;
 	
 	full_adder f_a1(s[0], c1, a[0], (b[0]^mode), mode);
 	full_adder f_a2(s[1], c2, a[1], (b[1]^mode), c1);
 	full_adder f_a3(s[2], c3, a[2], (b[2]^mode), c2);
-	full_adder f_a4(s[3], cout, a[3], (b[3]^mode), c3);
+	full_adder f_a4(s[3], c4, a[3], (b[3]^mode), c3);
+	and a1(cout, ~c4, mode);
 
 endmodule
 
